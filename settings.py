78212fb4d3f3922958ffa6a5e9483a941d655e30
lao605 <zlao@redhat.com>
2015-11-06 17:59:10 +0800

    initial commit

---
+++ settings.py
@@ -0,0 +1,26 @@
+#
+# Copyright (c) 2015
+# Licensed under The MIT License (MIT)
+# http://opensource.org/licenses/MIT
+#
+
+douyu_url = "http://www.douyu.com/directory/game/How"
+panda_url = ""
+zhanqi_url = ""
+longzhu_url = ""
+
+douyu_streamers = [
+
+]
+
+panda_streamers = [
+
+]
+
+zhanqi_streamers = [
+
+]
+
+longzhu_streamers = [
+
+]
